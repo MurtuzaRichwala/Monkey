05401c06a996c9c3b8f0c72473fda0de0428b702
MurtuzaRichwala <74816569+MurtuzaRichwala@users.noreply.github.com>
2020-11-29 13:17:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+
+var monkey , monkey_running
+var banana ,bananaImage, obstacle, obstacleImage
+var FoodGroup, obstacleGroup
+var score
+var survivaltime=0
+
+function preload(){
+  
+  
+  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
+  
+  bananaImage = loadImage("banana.png");
+  obstaceImage = loadImage("obstacle.png");
+ 
+}
+
+
+
+function setup() {
+  createcanvas(400,400);
+  
+  monkey=createsprite(80,315,20,20);
+  monkey.addAnimation("moving",monkey_running);
+  monkey.scale=0.2;
+  
+  ground=createsprite(400,350,900,10);
+  ground.velocityX=-4;
+  ground.x=ground.width/2;
+  
+  
+
+  
+}
+
+
+function draw() {
+  
+  stroke("white");
+  textSize(20);
+  fill(white);
+  text("score:"+score,500,50);
+  
+  stroke("black")
+  textSize(20);
+  fill("black");
+  survivaltime=Math.ciel(frameCount/frameRate());
+  text("survivalTime:"+ survivalTime,100,50);
+  
+  
+  drawsprite();
+  
+  
+
+  
+}
+
+
+
+
+
+
